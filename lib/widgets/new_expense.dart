e7ec71a8be676fd8cb7754475238ad7df1a04ef0
Charl913 <charlkruger61@gmail.com>
2023-10-04 13:35:07 +0200

    Create Fullscreen Modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -73,6 +73,7 @@ class _NewExpensesState extends State<NewExpense> {
           date: _selectedDate!,
           category: _selectedCategory),
     );
+    Navigator.pop(context);
   }
 
   @override
@@ -85,7 +86,7 @@ class _NewExpensesState extends State<NewExpense> {
   @override
   Widget build(context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16,48,16,16),
       child: Column(
         children: [
           TextField(
